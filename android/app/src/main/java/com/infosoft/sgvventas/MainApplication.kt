ebb2e207ece0726669ad0ae2cf7eb1180b9d265e
Jorge Pirela <jorgepirelarivas@gmail.com>
2025-09-18 20:31:37 -0400

    android: remove Expo wrappers from MainActivity/MainApplication; build release OK

---
+++ android/app/src/main/java/com/infosoft/sgvventas/MainApplication.kt
@@ -0,0 +1,43 @@
+package com.infosoft.sgvventas
+
+import android.app.Application
+import android.content.res.Configuration
+
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.common.ReleaseLevel
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactNativeHost
+
+class MainApplication : Application(), ReactApplication {
+
+  override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
+    override fun getPackages(): List<ReactPackage> =
+      PackageList(this).packages.apply {
+        // add(MyReactNativePackage())
+      }
+
+    override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+
+    override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+    override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    DefaultNewArchitectureEntryPoint.releaseLevel = try {
+      ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
+    } catch (e: IllegalArgumentException) {
+      ReleaseLevel.STABLE
+    }
+    loadReactNative(this)
+  }
+
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+  }
+}
