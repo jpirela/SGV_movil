98e991028451f9244f914277050d418181dc4b01
Jorge Pirela <jorgepirelarivas@gmail.com>
2025-09-19 06:40:59 -0400

    chore(config-plugin): fully replace MainApplication.kt to drop Expo wrappers

--- plugins/removeExpoWrappers.js
+++ plugins/removeExpoWrappers.js
@@ -1,46 +1,62 @@
-const { withMainActivity, withMainApplication, AndroidConfig } = require('@expo/config-plugins');
-
-function replaceAll(source, replacements) {
-  let out = source;
-  for (const [pattern, replacement] of replacements) {
-    out = out.replace(pattern, replacement);
-  }
-  return out;
-}
+const { withMainActivity, withMainApplication } = require('@expo/config-plugins');
 
 const plugin = (config) => {
   // Patch MainActivity.kt: remove Expo ReactActivityDelegateWrapper usage
   config = withMainActivity(config, (cfg) => {
     const src = cfg.modResults.contents;
-    let next = src;
-    next = replaceAll(next, [
-      [/^import\s+expo\.modules\.ReactActivityDelegateWrapper\s*\r?\n/m, ''],
-      [
-        /return\s+ReactActivityDelegateWrapper\([\s\S]*?\)\s*\r?\n\s*\}/m,
-        'return object : DefaultReactActivityDelegate(\n      this,\n      mainComponentName,\n      fabricEnabled\n    ) {}\n  }\n',
-      ],
-    ]);
+    let next = src.replace(/^[\t ]*import\s+expo\.modules\.ReactActivityDelegateWrapper\s*\r?\n/m, '');
+    next = next.replace(
+      /return\s+ReactActivityDelegateWrapper\([\s\S]*?\)\s*\r?\n\s*\}/m,
+      'return object : DefaultReactActivityDelegate(\n      this,\n      mainComponentName,\n      fabricEnabled\n    ) {}\n  }\n'
+    );
     cfg.modResults.contents = next;
     return cfg;
   });
 
-  // Patch MainApplication.kt: remove Expo wrappers and dispatcher
+  // Overwrite MainApplication.kt with a wrapper-free template using the same package name
   config = withMainApplication(config, (cfg) => {
-    let src = cfg.modResults.contents;
-    let next = src;
-    next = replaceAll(next, [
-      [/^import\s+expo\.modules\.ApplicationLifecycleDispatcher\s*\r?\n/m, ''],
-      [/^import\s+expo\.modules\.ReactNativeHostWrapper\s*\r?\n/m, ''],
-      [/^import\s+com\.facebook\.react\.ReactHost\s*\r?\n/m, ''],
-      [
-        /override\s+val\s+reactNativeHost:[\s\S]*?\)\s*\r?\n\s*\)\s*\r?\n/m,
-        'override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {\n        override fun getPackages(): List<ReactPackage> =\n            PackageList(this).packages\n        override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"\n        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG\n        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED\n      }\n\n',
-      ],
-      [/^\s*override\s+val\s+reactHost:[\s\S]*?\r?\n\s*\}\s*\r?\n/m, ''],
-      [/^\s*ApplicationLifecycleDispatcher\.onApplicationCreate\(this\)\s*\r?\n/m, ''],
-      [/^\s*ApplicationLifecycleDispatcher\.onConfigurationChanged\([\s\S]*?\)\s*\r?\n/m, ''],
-    ]);
-    cfg.modResults.contents = next;
+    const src = cfg.modResults.contents;
+    const pkgMatch = src.match(/^package\s+([\w\.]+)/m);
+    const pkg = pkgMatch ? pkgMatch[1] : 'com.example.app';
+    const template = `package ${pkg}
+
+import android.app.Application
+import android.content.res.Configuration
+
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.common.ReleaseLevel
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactNativeHost
+
+class MainApplication : Application(), ReactApplication {
+
+  override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
+    override fun getPackages(): List<ReactPackage> = PackageList(this).packages
+    override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+    override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+    override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    DefaultNewArchitectureEntryPoint.releaseLevel = try {
+      ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
+    } catch (e: IllegalArgumentException) {
+      ReleaseLevel.STABLE
+    }
+    loadReactNative(this)
+  }
+
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+  }
+}
+`;
+    cfg.modResults.contents = template;
     return cfg;
   });
 
